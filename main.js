9edd6fcd6cc41b0d4d15b10d59e459a43966ed25
umarcor <unai.martinezcorral@ehu.eus>
2020-10-03 19:04:18 +0200

    support Ubuntu 20.04

--- main.js
+++ main.js
@@ -7,8 +7,25 @@ const path = require('path');
 async function run() {
   try {
     const isWindows = process.platform == 'win32';
+    const isLinux = process.platform == 'linux';
     let backend = core.getInput('backend');
     if (backend === '') { backend = 'mcode' };
+
+    let osVersion = '';
+
+    if ( isLinux ) {
+      const options = {
+        listeners: {
+          stdout: (data) => { osVersion += data.toString().replace(/^\s+|\s+$/g, ''); }
+        }
+      };
+      if ( await exec.exec('sed', ['-n', 's/^VERSION_ID="\\(.*\\)".*/\\1/p', '/etc/os-release'], options) ) {
+        throw new Error(`Could not read os version ID from '/etc/os-release!`);
+      };
+      if ( osVersion !== '18.04' && osVersion !== '20.04' ) {
+        throw new Error(`Ubuntu version ${ osVersion.replace(/^\s+|\s+$/g, '') } is not supported!`);
+      }
+    }
 
     const url = 'https://github.com/ghdl/ghdl/releases/download/nightly/' + (isWindows ?
       (backend === 'llvm' ?
@@ -17,7 +34,7 @@ async function run() {
         'mingw-w64-i686-ghdl-mcode-ci-1-any.pkg.tar.zst'
       )
       :
-      'ghdl-gha-ubuntu-' + backend + '.tgz'
+      `ghdl-gha-ubuntu-${ osVersion }-${ backend }.tgz`
     );
     console.log('Package URL:', url);
 
@@ -51,12 +68,22 @@ async function run() {
 
       await exec.exec('tar', ['-xvf', pkg], {cwd: dest});
 
-      // FIXME: without libgnat-7 the following error is produced:
-      // ghdl: error while loading shared libraries: libgnarl-7.so.1: cannot open shared object file: No such file or directory
+      // For 18.04
+      let gnatVersion = '7';
+      let llvmVersion = '5.0';
+
+      if ( osVersion == '20.04' ) {
+        gnatVersion = '9';
+        llvmVersion = '10';
+      }
+
+      // FIXME: without libgnat-* the following error is produced:
+      // ghdl: error while loading shared libraries: libgnarl-*.so.1: cannot open shared object file: No such file or directory
       // See https://github.com/ghdl/docker/issues/9
-      var pkgs = ['libgnat-7']
+      let pkgs = ['libgnat-' + gnatVersion]
+
       if (backend == 'llvm') {
-        pkgs += ['libllvm5.0']
+        pkgs.concat(['libllvm' + llvmVersion])
       }
       await exec.exec('sudo', ['apt', 'install', '-y'].concat(pkgs));
     }
